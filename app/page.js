fa7a54fa7ddbc6c783facb049da5f6dc24eac36e
asiasAtWork <asiasatwork@gmail.com>
2023-09-07 23:53:47 -0500

    Add starter sign up content

--- app/page.js
+++ app/page.js
@@ -2,8 +2,19 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="bg-stone-50 w-screen h-screen">
+      <h1>Sign Up</h1>
+      <p>Lorem ipsum dolor sit amet consectetur. Sit nisi euismod molestie pharetra porttitor varius aliquet sed.</p>
+      <p>Already have an account? <a>Log in.</a></p>
+      <button>Sign Up with Google</button>
+      <div>
+        <hr />
+        Or
+        <hr />
+      </div>
+      <form>
+        
+      </form>
     </div>
   )
 }
